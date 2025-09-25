23327966d15818faac6f01a53f8bc7cc21e74f30
Damonx34 <qletessier@yahoo.com>
2025-09-25 21:18:04 +0200

    numb

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello GitHub!")
+# main.py
+
+# Demande à l'utilisateur d'entrer des nombres séparés par des espaces
+numbers = input("Entrez des nombres séparés par des espaces : ")
+
+# Convertit la chaîne en liste de nombres
+numbers_list = [float(n) for n in numbers.split()]
+
+# Calcule la somme
+total = sum(numbers_list)
+
+# Affiche le résultat
+print("La somme des nombres est :", total)
