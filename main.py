d0f52de56b413c9596902e7829f7f0418526f7d2
Damonx34 <qletessier@yahoo.com>
2025-09-26 14:16:53 +0200

    litre

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def convertir_distance(valeur, unite_source, unite_cible):
-    # Convertir la valeur en mètres
     if unite_source == "m":
         valeur_m = valeur
     elif unite_source == "cm":
@@ -8,7 +7,6 @@ def convertir_distance(valeur, unite_source, unite_cible):
         valeur_m = valeur * 1000
     else:
         return "Unité source inconnue"
-
 
     if unite_cible == "m":
         return valeur_m
@@ -19,12 +17,47 @@ def convertir_distance(valeur, unite_source, unite_cible):
     else:
         return "Unité cible inconnue"
 
-valeur = float(input("Entrez la distance à convertir : "))
 
-unite_source = input("Entrez l'unité source (m/cm/km) : ")
+def convertir_volume(valeur, unite_source, unite_cible):
+    if unite_source == "l":
+        valeur_l = valeur
+    elif unite_source == "cl":
+        valeur_l = valeur / 100
+    elif unite_source == "ml":
+        valeur_l = valeur / 1000
+    else:
+        return "Unité source inconnue"
 
-unite_cible = input("Entrez l'unité cible (m/cm/km) : ")
+    if unite_cible == "l":
+        return valeur_l
+    elif unite_cible == "cl":
+        return valeur_l * 100
+    elif unite_cible == "ml":
+        return valeur_l * 1000
+    else:
+        return "Unité cible inconnue"
 
-resultat = convertir_distance(valeur, unite_source, unite_cible)
+
+# --- Programme principal ---
+print("Choisissez le type de conversion")
+print("1 - Distance (m, cm, km)")
+print("2 - Volume (l, cl, ml)")
+
+choix = input("Entrez 1 ou 2 : ")
+
+valeur = float(input("Entrez la valeur à convertir : "))
+
+if choix == "1":
+    unite_source = input("Entrez l'unité source (m/cm/km) : ")
+    unite_cible = input("Entrez l'unité cible (m/cm/km) : ")
+    resultat = convertir_distance(valeur, unite_source, unite_cible)
+
+elif choix == "2":
+    unite_source = input("Entrez l'unité source (l/cl/ml) : ")
+    unite_cible = input("Entrez l'unité cible (l/cl/ml) : ")
+    resultat = convertir_volume(valeur, unite_source, unite_cible)
+
+else:
+    resultat = "Type de conversion inconnu"
 
 print("Résultat :", resultat, unite_cible)
